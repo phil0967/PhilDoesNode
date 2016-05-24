d1a89e7ae505883203b8fc0e41b209f8d6ca7dce
Philip <p.birlund@hotmail.com>
2016-05-24 22:45:36 +0200

    work please

---
+++ README.md
@@ -0,0 +1,4 @@
+README file
+
+Mostly needed to tell other people how to install stuff
+or in this case allow me to use the commandline -nmp install 
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Express
 var express = require('express');
 var app = express();
+var users = require('./node_modules/routes/users.js');
+var cors = require('cors');
+app.use(cors());
 
 // Mongodb
 var mongodb = require('mongodb');
@@ -13,62 +16,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-// Generic API
-var genericApi = require('./generic-api')(mongodb, url);
-
-
-var apis = ["user", "product", "order"];
-
-for(var i = 0; i<apis.length; i++) {
-    var api = apis[i];
-    console.log(api);
-    app.get(`/${api}`, function (req, res) {
-        genericApi.get(api, function (err, data) {
-            res.send(data);
-        });
-    });
-
-    app.get(`/${api}/:id`, function (req, res) {
-        genericApi.getById(api, req.params.id, function (err, data) {
-            res.send(data);
-        });
-    });
-
-    app.post(`/${api}`, function (req, res) {
-        genericApi.postObject(api, req.body, function () {
-            res.status(200);
-            res.send();
-        });
-    });
-
-    app.delete(`/${api}`, function (req, res) {
-        genericApi.deleteObject(api, req.body._id, function (err, data) {
-            res.status(err == null ? 400 : 200);
-            res.send();
-        });
-    });
-
-    /**
-     *  Example PUT:
-     *  {
-     *      "where": {
-     *          "_id": "5723138cf81d4aeb48dbb235"
-     *      },
-     *      "set": {
-     *          "$set": {
-     *              "username":" Fancy"
-     *          }
-     *      }
-     *  }
-     */
-    app.put(`/${api}`, function (req, res) {
-        genericApi.putObject(api, req.body.where._id, req.body.set, function (err, data) {
-            res.status(err == null ? 400 : 200);
-            res.send();
-        });
-    });
-}
-
+app.use('/api/', require('./node_modules/routes/users.js'))
 
 
 
